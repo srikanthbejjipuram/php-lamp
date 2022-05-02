e8cbdab4d8ae4851687752de6109d43cb391a811
srikanthbejjipuram <77537737+srikanthbejjipuram@users.noreply.github.com>
2022-05-02 15:31:47 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,9 +1,9 @@
 <?php
 
 function Createdb(){
-    $servername = "localhost";
+    $servername = "backend";
     $username = "root";
-    $password = "";
+    $password = "root123";
     $dbname = "bookstore";
 
     // create connection
